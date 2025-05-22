8fc88946f523d202f0161a5113e439db55735040
snghhrsmrn <snghhrsmrn@gmail.com>
2025-05-22 14:05:16 +0530

    organize files

---
+++ organize_files.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+
+destination = {
+    "documents": ['.txt', '.doc', '.docx', '.pdf', 'xlsx', 'pptx', 'ods'],
+    "images": ['.png', '.jpg', '.jpeg', '.gif'],
+    "videos": ['.mp4'],
+    "torrents": ['.torrent'],
+    "applications": ['.exe'],
+    "archives": ['.zip', '.rar', '.tar', '.7zip']
+}
+
+def folder_sorter(download_path):
+    for file in os.listdir(download_path):
+        file_path = os.path.join(download_path, file)
+        if os.path.isfile(file_path):
+            for folder, extensions in destination.items():
+                if file.endswith(tuple(extensions)):
+                    folder_path = os.path.join(download_path, folder)
+                    os.makedirs(folder_path, exist_ok=True)
+                    shutil.move(file_path, folder_path)
+    print("Folder sorted")
+
+download_path = input("Enter the path here: ")
+folder_sorter(download_path)
